776e55c8b933f5155fe97846e13d488bd4818d4a
MariusFogelgren <marius.fogelgren@lego.com>
2024-09-12 08:56:12 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace EvenOROdd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Pnumber;
+            Console.WriteLine("Enter a number to check if it is even or odd");
+            Pnumber = Convert.ToInt32(Console.ReadLine());
+
+            if (Pnumber % 2 == 0)
+            {
+                Console.WriteLine("The number is even");
+            }
+            else
+            {
+                Console.WriteLine("The number is odd");
+            }
+
+        }
+    }
+}
