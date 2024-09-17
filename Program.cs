f59a3d3037a391773f01302ee78b6abd22d6df9a
MariusFogelgren <marius.fogelgren@lego.com>
2024-09-17 09:29:26 +0200

    Updated Code
    Easier to read.

--- Program.cs
+++ Program.cs
@@ -1,46 +1,75 @@
-﻿using System;
+using System;
 
-namespace MyConsoleApp
+namespace Game
 {
     class Program
     {
         static void Main(string[] args)
         {
             Console.ForegroundColor = ConsoleColor.DarkBlue;
-            Console.WriteLine("Welcome to the guessing game UUUUUAAAAAAA!");
+            Console.WriteLine("Welcome to the guessing game!");
+            GuessingGame game = new GuessingGame();
 
 
-            Random random = new Random();
-            int Rnumber = random.Next(1, 2);
-            int guess = 0;
-            int attempts = 0;
 
             Console.ForegroundColor = ConsoleColor.DarkYellow;
             Console.WriteLine("I have a number between 1 and 25. Can you guess it?");
 
-            while (guess != Rnumber)
+            bool didIWin = false;
+           
+
+            while (didIWin == false)
             {
                 Console.ForegroundColor = ConsoleColor.Gray;
                 Console.Write("Enter your guess: ");
-                guess = Convert.ToInt32(Console.ReadLine());
-                attempts++;
-            }
+                int guess = Convert.ToInt32(Console.ReadLine());
+                didIWin = game.MakeAGuess(guess);
 
 
-           
+                if (didIWin)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("You guessed correctly!");
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.WriteLine("It took you {0} attempts", game.Attempt);
+                    Console.ForegroundColor = ConsoleColor.Black;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("You guessed wrong!");
+                    Console.ForegroundColor = ConsoleColor.Black;
+                }
+            }
+        }
+    }
+}
 
-            if (guess == Rnumber)
-            {
-                Console.Clear();
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine("Congrats on the WIN!");
-                Console.ForegroundColor= ConsoleColor.Blue;
-                Console.WriteLine("The number was:{0}", Rnumber);
-                Console.ForegroundColor = ConsoleColor.Blue;
-                Console.WriteLine("It took you {0} attempts", attempts);
-                Console.ForegroundColor = ConsoleColor.Black;
-            }
-            }
+
+public class GuessingGame
+{
+
+    public int Attempt { get; private set; }
+    public int Rnumber;
+    public GuessingGame()
+    {
+        Attempt = 0;
+        Random random = new Random();
+        Rnumber = random.Next(1, 25);
+    }
+
+    public bool MakeAGuess(int PlayerGuess)
+    {
+        
+        Attempt = Attempt+1;
+        
+        if (PlayerGuess == Rnumber)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
         }
     }
 }
